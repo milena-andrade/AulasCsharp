5d9732b9dbec46e78e39eb32a28955866246635e
Milena Andrade <81273891+milena-andrade@users.noreply.github.com>
2023-08-19 14:49:56 -0300

    Add files via upload

---
+++ facul/Aluno.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace facul
+{
+    public class Aluno
+    {
+        /*Implemente uma classe chamada Aluno para definir os objetos que representarão os alunos de uma
+escola. Essa classe deve declarar três atributos: o primeiro para o nome, o segundo para o RG e o
+terceiro para a data de nascimento dos alunos.*/
+
+    /*Faça uma classe chamada TestaAluno e crie dois objetos da classe Aluno atribuindo valores a eles. A
+classe também deve mostrar na tela as informações desses objetos.*/
+
+        public string nome;
+        public string rg;
+        public date nascimento;
+
+
+    }
+}
---
+++ facul/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace facul{
+
+    class Program{
+        static void Main(string[] args){
+            Aluno aluno1 = new Aluno();
+            aluno1.nome = "Letheus";
+            aluno1.rg = "52.527.302-5";
+
+
+
+            Aluno aluno2 = new Aluno();
+            aluno2.nome = "A";
+            aluno2.rg = "43.473.213-0";
+
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
